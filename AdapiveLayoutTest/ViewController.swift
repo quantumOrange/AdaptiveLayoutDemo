fdb247b6efc4e6062a7a4c72dd13d1f1714b50ec
David Crooks <crodave@gmail.com>
2017-02-18 12:09:46 +0100

    Initial Commit

---
+++ AdapiveLayoutTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdapiveLayoutTest
+//
+//  Created by David Crooks on 18/02/2017.
+//  Copyright © 2017 David Crooks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
